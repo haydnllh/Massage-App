c78156d69d539b011f9468b502d4b1aed965a765
haydnllh <haydnllh719@gmail.com>
2025-06-15 14:29:22 +0100

    Login page

--- admin/src/Pages/Login/Login.jsx
+++ admin/src/Pages/Login/Login.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      <h1 className="heading center">Login</h1>
+
+      <div className="form-wrapper">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+          
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
